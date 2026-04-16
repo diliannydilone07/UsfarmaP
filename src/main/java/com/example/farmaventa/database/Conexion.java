a99edcdddd4fb3c4d887000f2a45fc50e01784a1
diliannydilone07 <diliannydilone@gmail.com>
2026-04-15 22:28:25 -0400

    hola bb te amo

--- src/main/java/com/example/farmaventa/database/Conexion.java
+++ src/main/java/com/example/farmaventa/database/Conexion.java
@@ -1,10 +1,7 @@
 package com.example.farmaventa.database;
 
-import javax.swing.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 public class Conexion {
     Connection connection = null;
@@ -15,16 +12,15 @@ public class Conexion {
     String server = "localhost";
     String puerto = "1433";
 
-    String cadena = "jdbc:sqlserver://" + server + "." + puerto + "/" + db;
-
     public Connection establecerConexion() {
         try {
-            String cadena = "jdbc:sqlserver://" + server + ":" + puerto + ";" + "databaseName=" + db + ";" + "encrypt=true" + ";" + "trustServerCertificate=true";
+            String cadena = "jdbc:sqlserver://" + server + ":" + puerto + ";"
+                    + "databaseName=" + db + ";"
+                    + "encrypt=true;"
+                    + "trustServerCertificate=true";
             connection = DriverManager.getConnection(cadena, usuario, contrase);
-            JOptionPane.showMessageDialog(null, "Se conecto correctamente la conexion a la bd");
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error en la conexion a la bd" + e.toString());
-
+            System.err.println("Error en la conexion a la BD: " + e.getMessage());
         }
         return connection;
     }
